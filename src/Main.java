ae1ea1e2f8e98879ee581453321c5695e89efe36
BenMeunier <158851537+BenMeunier@users.noreply.github.com>
2024-02-21 11:55:48 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class TemperatureConverter {
+class CtoFConverts {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double celsius;
